746fbca5c1d291d01e50c78207a3a90b583c09fb
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2021-10-31 13:33:31 +0200

    change build

--- runtimes/8.0/bin/install.sh
+++ runtimes/8.0/bin/install.sh
@@ -7,12 +7,10 @@ fi
 /bin/bash ./runtimes/8.0/bin/init-dummy-certs.sh
 
 cp .env.example .env
-docker run --rm -u "$(id -u):$(id -g)" -v $(pwd):/opt -w /opt  laravelsail/php80-composer:latest composer install --ignore-platform-reqs
+docker run --rm -u "$(id -u):$(id -g)" -v $(pwd):/opt -w /opt laravelsail/php80-composer:latest composer install --ignore-platform-reqs
 docker run --rm -u "$(id -u):$(id -g)" -v $(pwd):/opt -w /opt laravelsail/php80-composer:latest php artisan key:generate
 
 ./vendor/bin/sail up -d
 ./vendor/bin/sail npm install
 ./vendor/bin/sail npm run dev
-
-
 
